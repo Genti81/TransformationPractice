4d5e77e7b99136545d9035a6c10fe35e2f73c8d1
Gentrit Sahiti <sahiti.gentrit@gmail.com>
2017-11-01 23:35:05 +0100

    Update TransformTests.cs

--- TransformTestsLib/TransformTests.cs
+++ TransformTestsLib/TransformTests.cs
@@ -14,7 +14,7 @@ namespace TransformTestsLib
             var amount = "C#;2018-01-01;2018-02-05";
             //var equal = "Kursen C# pågår i 35 dagar med följande deltagare:";
             var tuple = new Tuple<string, DateTime, DateTime>("C#", DateTime.Parse("2018-01-01"), DateTime.Parse("2018-02-05"));
-            var tuple1 = new Tuple<string, List<string>>("Alice", new List<string>() { "C#", "SQL", "MVC" });
+            var tuple1 = new Tuple<string, string, List<string>>("Alice", "Carol", new List<string>() { "C#", "SQL", "MVC" });
 
             // tuple
             var kurs = tuple.Item1;
@@ -23,10 +23,16 @@ namespace TransformTestsLib
             // tuple1
             var name = tuple1.Item1;
             //var kursen = tuple1.Item2.Contains("C#");
-            var kursen = tuple1.Item2[0];
+            //Debug.WriteLine(kursen);
 
-            Debug.WriteLine(kursen);
-            var newLine = $"Krsen {kursen} pågår i {difDates} dagar med följande deltagare: {name}";
+            var kursen = tuple1.Item2[0];
+            var kursen1 = tuple1.Item2[1];
+            var dagar = int.Parse((difDates).TotalDays.ToString());
+
+            var newLine = $"Krsen {kursen} pågår i {dagar} dagar med följande deltagare: {name}";
+            var newLine = $"Krsen {kursen} pågår i {dagar} dagar med följande deltagare: {name}";
+
+            Debug.WriteLine(newLine);
 
 
         }
